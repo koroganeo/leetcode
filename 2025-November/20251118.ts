c0c5e98effbe2688291927219f627f6e5a545b0c
koroganeo <tnguyen.neo@gmail.com>
2025-11-18 22:27:21 +0700

    Nov 18, 2025

---
+++ 2025-November/20251118.ts
@@ -0,0 +1,7 @@
+function isOneBitCharacter(bits: number[]): boolean {
+  for (let i = 0; i < bits.length; i++) {
+    if (i === bits.length - 1) return true;
+    if (bits[i] === 1) i++;
+  }
+  return false;
+}
